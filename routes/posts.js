f99d944f8b5099eadef06edbaa679b5d7ab33526
Kim Pillseon <pillsun00@naver.com>
2023-06-14 16:04:57 +0900

    게시물 수정기능 구현

--- routes/posts.js
+++ routes/posts.js
@@ -32,4 +32,43 @@ router.get('/posts', async (req, res) => {
   res.json({ data });
 });
 
+router.get('/posts/:_postId', async (req, res) => {
+  const { _postId } = req.params;
+  const results = await Post.find({ postId: _postId });
+  const data = results.map((item) => {
+    return {
+      postId: item.postId,
+      user: item.user,
+      title: item.title,
+      content: item.content,
+      creataAt: item.createdAt,
+    };
+  });
+
+  res.json({ data });
+});
+
+router.put('/posts/:_postId', async (req, res) => {
+  const { _postId } = req.params;
+  const { password, user, title, content } = req.body;
+  const results = await Post.find({ postId: _postId });
+  if (results.length) {
+    const post = results[0];
+    if (post.password === password) {
+      await Post.updateOne({ postId: _postId }, { user, title, content });
+    } else {
+      res.status(400).json({
+        success: false,
+        errorMessage: '비밀번호가 일치하지 않습니다.',
+      });
+    }
+  } else {
+    return res.status(400).json({
+      success: false,
+      errorMessage: '게시물 조회에 실패했습니다.',
+    });
+  }
+
+  res.send('success');
+});
 module.exports = router;
